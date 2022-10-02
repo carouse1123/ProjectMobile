1a0a62a79addcd9325e43c3f730d6bdfc6664f54
carouse1123 <Theeraphong.s@kkumail.com>
2022-10-03 03:32:48 +0700

    login

--- app/src/main/java/com/example/testtt123/login.kt
+++ app/src/main/java/com/example/testtt123/login.kt
@@ -2,10 +2,19 @@ package com.example.testtt123
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class login : AppCompatActivity() {
+    var etusername: EditText? = null
+    var etpassword: EditText? = null
+    var btnlogin: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        etusername = findViewById(R.id.etUsername)
+        etpassword = findViewById(R.id.etPassword)
+        btnlogin = findViewById(R.id.btnLogin)
+
     }
 }
---
+++ app/src/main/java/com/example/testtt123/musicAPI.kt
@@ -0,0 +1,4 @@
+package com.example.testtt123
+
+interface musicAPI {
+}
